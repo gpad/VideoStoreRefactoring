b2e77e7bd801f36f75958fa349e1c40ca5a9e9dc
Dimitrios Palioselitis <dim.palioselitis@gmail.com>
2020-05-27 17:21:59 +0200

    Move getCharge method to Movie class

--- Movie.h
+++ Movie.h
@@ -12,6 +12,7 @@ Movie(const std::string &title, const int priceCode = 0);
 void setPriceCode(const int priceCode);
 int getPriceCode() const;
 std::string getTitle() const;
+double getCharge(int daysRented) const;
 
 private:
 int priceCode;
